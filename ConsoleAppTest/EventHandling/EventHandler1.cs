9f5c3de238b643746da668f5ef04c293c8b5414a
James Lee <james.lee3@utsa.edu>
2017-11-16 13:34:08 -0600

    ..

---
+++ ConsoleAppTest/EventHandling/EventHandler1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleAppTest.EventHandling
+{
+    public class EventHandler1
+    {
+        public event EventHandler EventHasBeenRaised;
+        public void RaiseAnEvent()
+        {
+            Console.WriteLine(this + ": event raised");
+            new 
+        }
+    }
+}
